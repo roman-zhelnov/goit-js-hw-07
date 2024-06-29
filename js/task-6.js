f5baf6e8fa6e4c9fb42f83389ef75a2e105a057d
Roman Zhelnov <roman95zhelnov@gmail.com>
2024-06-29 13:27:36 +0300

    add

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,35 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const controls = document.querySelector("#controls");
+const input = controls.querySelector("input");
+const buttonCreate = controls.querySelector("[data-create]");
+const buttonDestroy = controls.querySelector("[data-destroy]");
+const boxes = document.querySelector("#boxes");
+
+function createBoxes(amount) {
+  boxes.innerHTML = "";
+  for (let i = 0; i < amount; i++) {
+    const div = document.createElement("div");
+    const size = 30 + i * 10;
+    div.style.width = `${size}px`;
+    div.style.height = `${size}px`;
+    div.style.backgroundColor = getRandomHexColor();
+    boxes.append(div);
+  }
+}
+
+buttonCreate.addEventListener("click", madeButtonCreate);
+
+function madeButtonCreate() {
+  const amount = Number(input.value);
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+  }
+  input.value = "";
+}
+
+buttonDestroy.addEventListener("click", () => {
+  boxes.innerHTML = "";
+});
